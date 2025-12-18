df1a4274f4c39b200e5990d77aaadbbfdf4ccde2
Dovydas Venckus <dovven2@ktu.lt>
2025-12-19 01:59:01 +0200

    show data in /admin/

--- SportsNewsApp/admin.py
+++ SportsNewsApp/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import *
 
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "role", "is_staff", "is_superuser")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role", {"fields": ("role",)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Category, CategoryAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "category", "author")
+    search_fields = ("title", "text")
+    list_filter = ("category", "author")
+    
+admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "post", "author")
+    search_fields = ("text",)
+    list_filter = ("author", "post")
+
+admin.site.register(Comment, CommentAdmin)
